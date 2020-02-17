4b59bb7057f4f84b45aa9b0aa72056d337894757
Will Baker <willbaker698@gmail.com>
2020-02-17 09:18:11 +0000

    Index user authentication

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,15 @@
 <?php
 
+if (isset($data["username"])) {
+    ?>
+    <h1> HI, <?= $data["username"] ?> </h1>
 
+    <?php
+} else {
+    ?>
 
+    <h1> PLEASE <a href=""LOGIN </h1>
+    <?php
+}
 ?>
+
